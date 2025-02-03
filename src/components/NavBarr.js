91c6161065089d0339f146d0f830ad9d9b08e1d5
Wassim <wassim.besghayer@gmail.com>
2025-02-03 22:27:40 +0100

    React components

---
+++ src/components/NavBarr.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBarr() {
+  return (
+    <div className='nav-bar'>
+      <img src='https://cdn-icons-png.flaticon.com/512/11743/11743656.png'/>
+      {/* <img src='https://w7.pngwing.com/pngs/893/48/png-transparent-task-project-management-computer-icons-service-others-miscellaneous-text-logo.png'/> */}
+      <h1>To-Do List App</h1>
+    </div>
+  )
+}
+
+export default NavBarr
