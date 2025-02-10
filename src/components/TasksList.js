6f30afed335c854b2b3dccb46dc6d34451be70fb
Wassim <wassim.besghayer@gmail.com>
2025-02-10 14:22:37 +0100

    rfce instead of rafce

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import tasks from '../redux/taskSlice'
+import Task from './Task';
+
 
 function TasksList() {
+  const tasks = useSelector(state => state.task.tasksList);
+  console.log(tasks)
   return (
-    <div>TasksList</div>
+    <div>
+      {tasks.map((task)=>(
+        <Task task={task}/>))}
+    </div>
   )
 }
 
